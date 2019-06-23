409a1cc16f7c618c843f41691026645ab3c1a0d2
Abdessalam BENHARIRA <belmehelabdessalam.benharira@ynov.com>
2019-06-23 16:13:05 +0200

    feat: add accounts user form

--- src/pages/dashboard/servers/AccountsPage.vue
+++ src/pages/dashboard/servers/AccountsPage.vue
@@ -2,8 +2,90 @@
 	<div class="dashboard">
 		<div class="inner-dashboard">
 			<div class="dashboard-content" v-if="!isLoading">
-				<h1>Utilisateurs du serveur</h1>
+				<div class="dashboard-header">
+					<h1>Serveurs</h1>
+					<ul class="actions">
+						<li>
+							<a class="btn add-user" @click.prevent="showModal">
+								<i class="icon ion-ios-add-circle"></i>Ajouter
+								un utilisateur
+							</a>
+						</li>
+					</ul>
+				</div>
 				<div class="dashboard-section">
+					<modal v-show="isModalVisible" @close="closeModal">
+						<template #header>
+							<div>
+								Ajouter un utilisateur
+							</div>
+						</template>
+						<template #body>
+							<form class="add-user-form">
+								<div class="field">
+									<label class="label"
+										>Nom de l'utilisateur</label
+									>
+									<input
+										class="input"
+										:class="{
+											'has-error': errors.username
+										}"
+										required
+										v-model="fields.username"
+										type="text"
+										@keydown="errors.username = ''"
+									/>
+									<p class="help error">
+										{{ errors.username }}
+									</p>
+								</div>
+								<div class="field">
+									<label class="label">Mot de passe</label>
+									<input
+										class="input"
+										:class="{
+											'has-error': errors.password
+										}"
+										required
+										v-model="fields.password"
+										type="password"
+										@keydown="errors.password = ''"
+									/>
+									<p class="help error">
+										{{ errors.password }}
+									</p>
+								</div>
+							</form>
+						</template>
+						<template #footer>
+							<div>
+								<button
+									class="btn cancel"
+									@keyup.27="closeModal"
+									@click="closeModal"
+								>
+									Annuler
+								</button>
+								<button
+									@click.prevent="addUser"
+									class="btn submit"
+									:disabled="addPending"
+								>
+									<span v-if="addPending === false">
+										Ajouter
+									</span>
+
+									<Loader
+										v-else
+										:strokeColor="'#fff'"
+										:width="'20'"
+										:height="'20'"
+									></Loader>
+								</button>
+							</div>
+						</template>
+					</modal>
 					<div>
 						<table class="table table-dashboard">
 							<thead>
@@ -50,6 +132,7 @@
 <script>
 import {debug, parseToObject, isArraysEqual, server} from "../../../utils";
 import Loader from "@/components/ui/loader";
+import Modal from "@/components/partials/Modal";
 import axios from "axios";
 import {apiUrl} from "../../../config";
 
@@ -58,16 +141,67 @@ export default {
 	data() {
 		return {
 			infos: {},
-			isLoading: true
+			isLoading: true,
+			errors: {},
+			isModalVisible: false,
+			addPending: false,
+			fields: {
+				username: "",
+				password: ""
+			}
 		};
 	},
 	components: {
-		Loader
+		Loader,
+		Modal
 	},
 	methods: {
 		retrieveInfos() {
 			// TODO: implement async/await functions
 			this.$socket.sendObj(server.getUsers());
+		},
+		showModal() {
+			this.isModalVisible = true;
+		},
+		closeModal() {
+			this.resetForm();
+			this.isModalVisible = false;
+		},
+		isFieldValid(value) {
+			if (!value.length) {
+				return false;
+			}
+			return true;
+		},
+		closeOnEscape(e) {
+			const key = e.which || e.keyCode || e.detail;
+			if (key === 27 && this.isModalVisible === true) {
+				this.closeModal();
+			}
+		},
+		addUser() {
+			this.errors = {};
+			for (const field in this.fields) {
+				if (!this.isFieldValid(this.fields[field])) {
+					this.errors[field] =
+						"Ce champs et requis et doit être valide";
+				}
+			}
+			if (Object.keys(parseToObject(this.errors)).length === 0) {
+				this.addPending = true;
+				setTimeout(() => {
+					this.addPending = false;
+					this.closeModal();
+					this.refreshData();
+					this.resetForm();
+				}, 1500);
+			}
+		},
+		resetForm() {
+			this.errors = {};
+			for (const field in this.fields) {
+				this.fields[field] = "";
+			}
 		},
 		fetchData(serverId) {
 			axios
@@ -105,6 +239,7 @@ export default {
 		}
 	},
 	created() {
+		window.addEventListener("keyup", this.closeOnEscape);
 		debug("info", "mounted -> this.dataServer", this.dataServer);
 		if (this.dataServer !== undefined) {
 			this.server = this.dataServer;
@@ -144,3 +279,139 @@ export default {
 	}
 };
 </script>
+
+<style lang="scss" scoped>
+.dashboard {
+	.inner-dashboard {
+		.dashboard-content {
+			.dashboard-header {
+				display: flex;
+				align-items: center;
+				justify-content: space-between;
+				margin-bottom: 30px;
+
+				h1 {
+					color: #303133;
+					margin-top: 0;
+					margin-bottom: 30px;
+				}
+
+				.actions {
+					padding: 0;
+					margin: 0;
+					list-style: none;
+
+					.add-user {
+						position: relative;
+						font-size: 14px;
+						padding: 12px;
+						display: flex;
+						align-items: center;
+						justify-content: center;
+
+						> .icon {
+							font-size: 18px;
+							margin-right: 10px;
+						}
+					}
+				}
+			}
+			form {
+				.field {
+					margin-bottom: 20px;
+					textarea {
+						height: 100px;
+						min-height: 100px;
+						width: 100%;
+					}
+				}
+			}
+
+			.modal {
+				form {
+					&.add-user-form {
+						padding: 0 20px;
+
+						.field {
+							position: relative;
+
+							* {
+								display: block;
+								width: 100%;
+								margin-bottom: 10px;
+							}
+
+							.input {
+								background: #fcfcfc;
+								border: 2px solid #e7e7e7;
+								margin: 10px auto;
+								padding: 10px 15px;
+								width: 100%;
+								display: block;
+								border-radius: 3px;
+								font-size: 16px;
+								transition: border-color ease-in-out 0.15s,
+									box-shadow ease-in-out 0.15s;
+								text-align: left;
+								height: auto;
+								-webkit-appearance: none;
+								outline: none;
+
+								&:hover {
+									border-color: rgba(39, 117, 237, 0.23);
+								}
+
+								&:focus {
+									border-color: #2874ed;
+								}
+
+								&.has-error {
+									border-color: #ff3860;
+								}
+							}
+
+							p.help {
+								margin: 0;
+								position: absolute;
+								bottom: -15px;
+								font-size: 12px;
+
+								&.error {
+									color: #ff3860;
+								}
+							}
+						}
+					}
+				}
+
+				.modal-footer {
+					> div {
+						display: flex;
+
+						button {
+							padding: 12px 20px;
+
+							&:first-child {
+								margin-right: 10px;
+							}
+
+							&.cancel {
+								color: #303133;
+								background: transparent;
+
+								&:hover {
+									background: #e7e7e7;
+								}
+							}
+
+							&.submit {
+								min-width: 90px;
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+}
+</style>
